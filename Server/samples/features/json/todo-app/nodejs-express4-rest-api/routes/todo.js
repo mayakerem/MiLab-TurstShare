2a051104777c1b788f419c8003e655ab395b62bb
Maya Kerem <mayakerem21@gmail.com>
2019-03-27 08:26:38 +0200

    added server

---
+++ Server/samples/features/json/todo-app/nodejs-express4-rest-api/routes/todo.js
@@ -0,0 +1,49 @@
+﻿var router = require('express').Router();
+var TYPES = require('tedious').TYPES;
+
+/* GET task listing. */
+router.get('/', function (req, res) {
+
+    req.sql("select * from drivers_db for json path")
+        .into(res, '[]');
+
+});
+
+/* GET single task. */
+router.get('/:id', function (req, res) {
+    
+    req.sql("select * from drivers_db where id = @id for json path, without_array_wrapper")
+        .param('id', req.params.id, TYPES.Int)
+        .into(res, '{}');
+
+});
+
+/* POST create task. */
+router.post('/', function (req, res) {
+    
+    req.sql("exec createDrivers @drivers_db")
+        .param('drivers_db', req.body, TYPES.NVarChar)
+        .exec(res);
+
+});
+
+/* PUT update task. */
+router.put('/:id', function (req, res) {
+    
+    req.sql("exec updateDrivers @id, @drivers_db")
+        .param('id', req.params.id, TYPES.Int)
+        .param('drivers_db', req.body, TYPES.NVarChar)
+        .exec(res);
+
+});
+
+/* DELETE single task. */
+router.delete('/:id', function (req, res) {
+    
+    req.sql("delete from drivers_db where id = @id")
+        .param('id', req.params.id, TYPES.Int)
+        .exec(res);
+
+});
+
+module.exports = router;
---
+++ Server/samples/tutorials/README.md
@@ -0,0 +1,16 @@
+# Database connection samples
+[Getting Started Tutorials](https://aka.ms/sqldev)
+
+Contains samples that show how to connect to Microsoft SQL databases, including SQL Server, Azure SQL Database, and Azure SQL Data Warehouse from different langauges like:
+* C#
+* C/C++
+* F#
+* Java
+* Node.js
+* PHP
+* Python
+* Ruby
+
+
+
+
