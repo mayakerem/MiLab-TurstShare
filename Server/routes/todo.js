9b26b00a9e56fe684f5f5a48a7b0001aef19195e
Maya Kerem <mayakerem21@gmail.com>
2019-03-30 22:31:57 +0300

    added SQL queries for pull data. checking push data doesnt work

--- Server/routes/todo.js
+++ Server/routes/todo.js
@@ -1,49 +1,70 @@
 ﻿var router = require('express').Router();
 var TYPES = require('tedious').TYPES;
 
-/* GET task listing. */
-router.get('/', function (req, res) {
+/* GET all drivers. */
+router.get('/drivers', function (req, res) {
 
     req.sql("select * from drivers_db for json path")
         .into(res, '[]');
-
 });
 
-/* GET single task. */
-router.get('/:id', function (req, res) {
+/* GET a driver with id as a phone number. */
+router.get('/drivers/:id', function (req, res) {
     
-    req.sql("select * from drivers_db where id = @id for json path, without_array_wrapper")
+    req.sql("select * from drivers_db where drivers_db.DriverID = @id for json path, without_array_wrapper")
         .param('id', req.params.id, TYPES.Int)
         .into(res, '{}');
-
 });
 
-/* POST create task. */
-router.post('/', function (req, res) {
+/* POST add a ride. */
+router.post('/drivers/:id/:name/:riderid', function (req, res) {
     
-    req.sql("exec createDrivers @drivers_db")
-        .param('drivers_db', req.body, TYPES.NVarChar)
+    req.sql("INSERT INTO drivers_db (DriverID, DriverName, RiderID) VALUES (@id, @name, @riderid)")
+        .param('id', 'name', 'riderid', req.body, TYPES.NVarChar)
         .exec(res);
-
 });
 
-/* PUT update task. */
-router.put('/:id', function (req, res) {
+/* GET all phonecontacts. */
+router.get('/phonecontacts', function (req, res) {
+
+    req.sql("select * from phonecontacts_db for json path")
+        .into(res, '[]');
+});
+
+/* GET all the phonecontacts from user id */
+router.get('/phonecontacts/:id', function (req, res) {
     
-    req.sql("exec updateDrivers @id, @drivers_db")
+    req.sql("select * from phonecontacts_db where phonecontacts_db.ownerid = @id for json path, without_array_wrapper")
         .param('id', req.params.id, TYPES.Int)
-        .param('drivers_db', req.body, TYPES.NVarChar)
-        .exec(res);
-
+        .into(res, '{}');
 });
 
-/* DELETE single task. */
-router.delete('/:id', function (req, res) {
+/* POST add a ride. */
+router.post('/phonecontacts/:id/:contact/:contactid', function (req, res) {
     
-    req.sql("delete from drivers_db where id = @id")
-        .param('id', req.params.id, TYPES.Int)
+    req.sql("INSERT INTO phonecontacts (ownerid, contactname, contactid) VALUES (@id, @contact, @contactid)")
+        .param('id', 'contact', 'contactid', req.body, TYPES.NVarChar)
         .exec(res);
-
 });
+
+
+// /* PUT update task. */
+// router.put('/:id', function (req, res) {
+    
+//     req.sql("exec updateDrivers @id, @drivers_db")
+//         .param('id', req.params.id, TYPES.Int)
+//         .param('drivers_db', req.body, TYPES.NVarChar)
+//         .exec(res);
+
+// });
+
+// /*  . */
+// router.delete('/:id', function (req, res) {
+    
+//     req.sql("delete from drivers_db where id = @id")
+//         .param('id', req.params.id, TYPES.Int)
+//         .exec(res);
+
+// });
 
 module.exports = router;
